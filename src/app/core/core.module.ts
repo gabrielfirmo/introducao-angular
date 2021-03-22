e284c36087ba29f20800bf3b8af0396ed32f9e26
gabrielfirmo <gabriel_firmo@hotmail.com>
2021-03-22 13:50:38 -0300

    fix: Redirecionamento para a rota courses

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -11,6 +11,9 @@ import { Error404Component } from './component/error-404/error-404.component';
     imports: [
         RouterModule.forChild([
             {
+                path: '', redirectTo: 'courses', pathMatch: 'full'
+            },
+            {
                 path:'**', component: Error404Component
             }
         ])
