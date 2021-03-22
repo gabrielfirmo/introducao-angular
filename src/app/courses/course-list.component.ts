5977e9faf46ad8ed2d8d9168f47c16c696ea12b8
gabrielfirmo <gabriel_firmo@hotmail.com>
2021-03-22 10:31:50 -0300

    feat: botão de delete

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -14,16 +14,26 @@ export class CourseListComponent implements OnInit{
     ngOnInit(): void{
         this.retrieveAll();
     }
-retrieveAll(): void{
-    this.courseService.retrieveAll().subscribe({
-        next: courses =>{
-            this._courses = courses;
-            this.filteredCourses = this._courses;
-        },
-        error: err => console.log('Error',err)
-    })
-        
-}
+    retrieveAll(): void{
+        this.courseService.retrieveAll().subscribe({
+            next: courses =>{
+                this._courses = courses;
+                this.filteredCourses = this._courses;
+            },
+            error: err => console.log('Error',err)
+        })
+            
+    }
+    
+    deleteById(courseId:number): void{
+        this.courseService.deleteById(courseId).subscribe({
+            next: () => {
+                console.log('Deleted with success');
+                this.retrieveAll();
+            },
+            error: err => console.log('Error', err)
+        })
+    }
 
     set filter(value: string){
         this._filterBy = value;
